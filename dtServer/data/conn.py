0f5c067abdf74796e1d421f035b09b914e5e8e67
jungil kim <jikim424@gmail.com>
2025-02-10 16:44:24 +0900

    Change conn.py

--- dtServer/data/conn.py
+++ dtServer/data/conn.py
@@ -6,7 +6,6 @@ db_proxy = DatabaseProxy()
 
 def init_db_conn() : 
     conn = make_database_connection()    
-    print()
 
 def create_connection(db, user, password, host, port) : 
     conn = MySQLDatabase(db, user = user, password = password, host = host, port=port)    
@@ -15,12 +14,12 @@ def create_connection(db, user, password, host, port) :
 
 def make_database_connection() -> MySQLDatabase : 
     load_dotenv()
-    user = os.environ.get('USER')
-    password = os.environ.get('PASSWORD')
-    host = os.environ.get('HOST')
-    port = int(os.environ.get('PORT'))
-    db = os.environ.get('DATABASE')
-
-    print(user) ##
+    user = os.getenv('USER')
+    password = os.getenv('PASSWORD')
+    host = os.getenv('HOST')
+    port = int(os.getenv('PORT'))
+    db = os.getenv('DATABASE')
 
     return create_connection(db, user, password, host, port)
+
+make_database_connection()
