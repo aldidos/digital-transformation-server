3841e38f5446309b0d2b713f4190128ed99f3a26
jungil kim <jikim424@gmail.com>
2025-02-12 16:36:03 +0900

    Fixed WorkoutMetricsDao

--- dtServer/data/dao/workout_metrics_dao.py
+++ dtServer/data/dao/workout_metrics_dao.py
@@ -4,6 +4,7 @@ from dtServer.data.model.workout_sessions import WorkoutSessions
 from dtServer.data.model.workouts import Workouts
 from dtServer.data.model.workout_set import WorkoutSet
 from dtServer.data.model.exercise_library import ExerciseLibrary
+from dtServer.data.model.exerciselib_bodypart import ExerciseLibBodyPart
 from dtServer.data.model.user import User
 from dtServer.data.model.body_part import BodyPart
 from playhouse.shortcuts import model_to_dict, dict_to_model
@@ -28,11 +29,12 @@ class WorkoutMetricsDao(BaseDAO) :
         q = WorkoutMetrics.select().join(WorkoutSet)\
             .join(Workouts)\
             .join(ExerciseLibrary)\
+            .join(ExerciseLibBodyPart)\
             .join(BodyPart)\
             .join_from(Workouts, WorkoutSessions)\
             .join_from(WorkoutSessions, User)\
-            .where(User.id == user_id, WorkoutSessions.is_completed == True, WorkoutSessions.date.between( from_date, to_date) )        
+            .where(User.id == user_id, WorkoutSessions.is_completed == True, WorkoutSessions.date.between( from_date, to_date) ) 
         list_data = [ row for row in q.dicts()]
-        return list_data        
+        return list_data  
     
 workoutMetricDao = WorkoutMetricsDao()
