34f53a93b0071b30e9128646b3782adbacdf30b9
jungil kim <jikim424@gmail.com>
2025-03-06 10:15:06 +0900

    Refactoring

--- dtServer/data/dao/workout/workout_report_data_dao.py
+++ dtServer/data/dao/workout/workout_report_data_dao.py
@@ -4,16 +4,7 @@ from dtServer.data.dao.workout.workout_metrics_dao import workoutMetricDao
 from dtServer.data.dao.workout.workouts_dao import workoutDao
 from dtServer.data.dao.workout.workout_sessions_dao import workoutSessionDao
 
-class WorkoutReportDao : 
-
-    def get_workout_set_metric_data(self, workoutset) : 
-        if not workoutset : 
-            return None, None
-        
-        workout_set_id = workoutset['id']
-        workout_id = workoutset['workout']['id']       
-        workout_metric = workoutMetricDao.select_by_workout_and_workoutset_id(workout_id, workout_set_id)            
-        return workoutset, workout_metric
+class WorkoutReportDao :     
     
     def get_workout_set_data(self, workout_set_id) : 
         with db_proxy.atomic() :
@@ -35,16 +26,6 @@ class WorkoutReportDao :
             workoutset = workoutSetDao.get_recent_by_exerciselibrary(user_id, exercise_library_id, set)
             return self.get_workout_set_metric_data(workoutset)   
     
-    def get_workout_data(self, workout_id) : 
-        with db_proxy.atomic() : 
-            workout_metrics = workoutMetricDao.select_by_workout_id(workout_id)
-            return workout_metrics
-
-    def get_workoutsession_data(self, workout_session_id) : 
-        with db_proxy.atomic() : 
-            workout_metrics = workoutMetricDao.select_by_workout_session_id(workout_session_id)
-            return workout_metrics
-        
     def get_recent_workout_data_by_exercise(self, user_id, exercise_library_id) : 
         with db_proxy.atomic() : 
             workouts = workoutDao.get_recent_by_exercise_library(user_id, exercise_library_id)
@@ -70,5 +51,24 @@ class WorkoutReportDao :
         with db_proxy.atomic() : 
             workout_metrics = workoutMetricDao.select_by_date_period(user_id, from_date, to_date)
             return workout_metrics
+        
+    def get_workout_data(self, workout_id) : 
+        with db_proxy.atomic() : 
+            workout_metrics = workoutMetricDao.select_by_workout_id(workout_id)
+            return workout_metrics
+
+    def get_workoutsession_data(self, workout_session_id) : 
+        with db_proxy.atomic() : 
+            workout_metrics = workoutMetricDao.select_by_workout_session_id(workout_session_id)
+            return workout_metrics
+        
+    def get_workout_set_metric_data(self, workoutset) : 
+        if not workoutset : 
+            return None, None
+        
+        workout_set_id = workoutset['id']
+        workout_id = workoutset['workout']['id']       
+        workout_metric = workoutMetricDao.select_by_workout_and_workoutset_id(workout_id, workout_set_id)            
+        return workoutset, workout_metric
     
 workoutReportDao = WorkoutReportDao()
